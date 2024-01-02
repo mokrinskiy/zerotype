ac1b01542bfff4ce934fd1af9c49e5dc753546fc
mokrinskiy <artyommokrinskiy@gmail.com>
2024-01-02 23:16:25 +0700

    footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Github, Instagram, Send } from "lucide-react";
+import React from "react";
 
 const Footer: React.FC = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <footer className="flex justify-between items-center p-5">
+            {/* logo */}
+            <h1 className="text-xl font-bold">zerotype</h1>
+            {/* social */}
+            <div className="max-md:flex-col flex items-start">
+                <Button variant="link" className="space-x-1">
+                    <Send />
+                    <p>Telegram</p>
+                </Button>
+                <Button variant="link" className="space-x-1">
+                    <Instagram />
+                    <p>Instagram</p>
+                </Button>
+                <Button variant="link" className="space-x-1">
+                    <Github />
+                    <p>GitHub</p>
+                </Button>
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -4,17 +4,15 @@ import React from "react";
 
 const NavBar: React.FC = () => {
     return (
-        <header className="p-5 h-[120px] flex justify-around items-center max-md:justify-between">
-            {/* settings button */}
+        <header className="p-5 h-[120px] max-md:h-[80px] flex justify-around items-center max-md:justify-between">
+            {/* pc nav */}
             <div className="max-md:hidden">
                 <Button>
                     <Settings className="md:mr-2" />
                     <p>Settings</p>
                 </Button>
             </div>
-            {/* logo */}
             <h1 className="text-4xl font-bold">zerotype</h1>
-            {/* user */}
             <div className="flex space-x-2 max-md:hidden">
                 <Button size="icon">ENG</Button>
                 <Button size="icon">
@@ -24,7 +22,7 @@ const NavBar: React.FC = () => {
                     <User />
                 </Button>
             </div>
-            {/* mobile buttons */}
+            {/* mobile nav */}
             <div className="md:hidden space-x-2">
                 <Button size="icon">
                     <Settings />
